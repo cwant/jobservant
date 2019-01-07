e8a6f58a044537b8086525d14c75d037837874a5
Chris Want <cjwant@gmail.com>
2019-01-06 17:16:48 -0700

    More love for the status table.

--- jobservant/jupyter/job_status_table.py
+++ jobservant/jupyter/job_status_table.py
@@ -5,47 +5,88 @@ class JobStatusTable:
 
     def __init__(self, cluster_job, **kwargs):
         self.cluster_job = cluster_job
+        self.html = None
+        self.should_separate_next_line = None
         self.is_table_style_set = False
+        self.section_class = None
 
     def run(self, *kwargs):
-        data = [['Cluster', self.cluster_job.cluster_account.server],
-                ['Username', self.cluster_job.cluster_account.username],
-                ['Job ID', self.cluster_job.jobid]]
+        self.start_table()
+        self.add_row('Cluster',
+                     self.cluster_job.cluster_account.server)
+        self.add_row('Username',
+                     self.cluster_job.cluster_account.username)
+        self.add_row('Job ID', self.cluster_job.jobid)
 
+        self.add_separator(section_class='job-status')
         queue_status = self.cluster_job.queue_status_hash()
         if queue_status == {}:
-            data += [['State', 'Finished']]
+            self.add_row('State', 'Finished')
         else:
-            data += [['State', queue_status['STATE']],
-                     ['Accounting group', queue_status['ACCOUNT']],
-                     ['Partition', queue_status['PARTITION']]]
-        self.render_vertical_table(data)
+            self.add_row('State', queue_status['STATE'])
 
-    def render_vertical_table(self, data_table):
+            self.add_separator()
+            self.add_row('Accounting group', queue_status['ACCOUNT'])
+            self.add_row('Partition', queue_status['PARTITION'])
+
+            self.add_separator()
+            self.add_row('Submit time', queue_status['SUBMIT_TIME'])
+            self.add_row('Start time', queue_status['START_TIME'])
+            self.add_row('End time', queue_status['END_TIME'])
+
+            self.add_separator()
+            self.add_row('Node list', queue_status['NODELIST'])
+
+        self.finish_table()
+
+    def start_table(self):
         self.setup_table_style()
 
-        # Each row in data_table has header, data, (optional) class
-        html = '<table>'
-        for row in data_table:
-            class_string = ''
-            if len(row) > 2:
-                class_string = ' class={}'.format(row[2])
-            header = '<th>{}</th>'.format(row[0])
-            data = '<td>{}</t1>'.format(row[1])
-            html += '<tr{}>{}{}</tr>'.format(class_string,
-                                             header,
-                                             data)
-        html += '</table>'
-        display(HTML(html))
+        self.html = '<table class="jobservant status-table">'
+        self.should_separate_next_line = False
+        self.section_class = None
+
+    def finish_table(self):
+        self.html += '</table>'
+        display(HTML(self.html))
+
+    def add_separator(self, **kwargs):
+        self.should_separate_next_line = True
+        self.section_class = kwargs.get('section_class')
+
+    def add_row(self, heading, data, **kwargs):
+        class_string = kwargs.get('class', '')
+        if self.should_separate_next_line:
+            class_string += ' line-above'
+            self.should_separate_next_line = False
+        if self.section_class:
+            class_string += ' {}'.format(self.section_class)
+        if len(class_string) > 0:
+            class_string = ' class="{}"'.format(class_string)
+        row = '<th>{}</th>'.format(heading) + \
+            '<td>{}</td>'.format(data)
+        self.html += '<tr{}>{}</tr>'.format(class_string, row)
 
     def setup_table_style(self):
         if self.is_table_style_set:
             return
         display(HTML("""
         <style>
-        .rendered_html td {
-          text-align: left;
-        }
+          .rendered_html th {
+            border-right: solid 1px #888;
+          }
+          .rendered_html td {
+            text-align: left;
+          }
+          .rendered_html tr.line-above {
+            border-top: 1px solid #888;
+          }
+          .rendered_html tbody tr:nth-child(odd):not(:hover) {
+            background: inherit;
+          }
+          .rendered_html tbody tr.job-status:not(:hover) {
+            background: #eeeeff;
+          }
         </style>
         """))
         self.is_table_style_set = True
